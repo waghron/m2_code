2fe720b1000616f94f3ad8d82573decf11fa0ecb
ihsah <ihorsahaydak@gmail.com>
2020-03-09 23:23:25 +0200

    init

--- Waghron/Base/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE,
-    'Waghron_Base',
-    __DIR__
-);
--- Waghron/IsometricGrid/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE,
-    'Waghron_IsometricGrid',
-    __DIR__
-);
